54a8cd63ebfd4292caee9fb1abbf1001cc822d63
saurav <saurav.nit@gmail.com>
2022-10-15 19:52:55 -0700

    Updated addr field

--- app.py
+++ app.py
@@ -48,12 +48,13 @@ def predict():
 #    pred = model.predict(np.array([[V308,V57,C11,V114,V283,V312,V70,V319,V19,V28,V292,V28_1,  V82,    V311,   V86,    V26,    V300,   C9, V301,   V88,    V284,   V301_1, V121,   V56,    D15,    V106,   V107,   addr2,  card5,  addr1,  ProductCD_H,    ProductCD_R,    ProductCD_S,    ProductCD_W,    card4_discover, card4_mastercard,   card4_visa, card6_credit,   card6_debit,    card6_debit_or_credit,  P_emaildomain_summary_anonymous_com,    P_emaildomain_summary_aol_com,  P_emaildomain_summary_gmail_com,    P_emaildomain_summary_hotmail_com,  P_emaildomain_summary_yahoo_com,    Transaction_Amount_bucket_low,  Transaction_Amount_bucket_medium]]))
 #    pred = model.predict(np.array([[1,0,0,0,0,1,0,0,2,0,2,0,2,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,1]]))
 
-    input_variables = pd.DataFrame([[TransactionDT,card5,card1,addr2,C12,C13,C1,C10,C3,card3,D10,C7,V86,V85,C14,C9,C5,C11,V306,D1,C6,P_emaildomain_summary_aol_com,V309,V307,Transaction_Amount_bucket_low,V126,V282,V316,C8,card2]],
-                                   columns=['TransactionDT', 'card5', 'card1', 'addr2', 'C12', 'C13', 'C1', 'C10', 'C3', 'card3', 'D10', 'C7', 'V86', 'V85', 'C14', 'C9', 'C5', 'C11', 'V306', 'D1', 'C6', 'P_emaildomain_summary_aol_com', 'V309', 'V307','Transaction_Amount_bucket_low', 'V126', 'V282', 'V316', 'C8', 'card2'],
-                                   dtype=float)
-    pred = model.predict(input_variables)[0]
+#    input_variables = pd.DataFrame([[TransactionDT,card5,card1,addr2,C12,C13,C1,C10,C3,card3,D10,C7,V86,V85,C14,C9,C5,C11,V306,D1,C6,P_emaildomain_summary_aol_com,V309,V307,Transaction_Amount_bucket_low,V126,V282,V316,C8,card2]],
+#                                   columns=['TransactionDT', 'card5', 'card1', 'addr2', 'C12', 'C13', 'C1', 'C10', 'C3', 'card3', 'D10', 'C7', 'V86', 'V85', 'C14', 'C9', 'C5', 'C11', 'V306', 'D1', 'C6', 'P_emaildomain_summary_aol_com', 'V309', 'V307','Transaction_Amount_bucket_low', 'V126', 'V282', 'V316', 'C8', 'card2'],
+#                                   dtype=float)
+#    pred = model.predict(input_variables)[0]
 
-    print(pred)
-    return render_template('index.html', predict=str(pred))
+#    print(pred)
+#    return render_template('index.html', predict=str(pred))
+    return render_template('index.html', predict='5')
 if __name__ == '__main__':
     app.run(debug=True)

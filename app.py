460680db74509d8108f70395a03b42faac47d671
saurav <saurav.nit@gmail.com>
2022-10-15 18:17:54 -0700

    Update random forest model and index/app file to support new model

--- app.py
+++ app.py
@@ -1,121 +1,55 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
-#import pickle
 import joblib
 app = Flask(__name__)
-filename = 'credit_card_fraud_detection_model_lr.sav'
-# model = pickle.load(open(filename, 'rb'))
+filename = 'credit_card_fraud_detection_model_rf.sav'
 model = joblib.load(filename)
-# model = joblib.load('filename.pkl')
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/predict', methods=['POST'])
 def predict():
-    V308 = request.form['V308']
-    V57 = request.form['V57']
-    C11 = request.form['C11']
-    V114 = request.form['V114']
-    V283 = request.form['V283']
-    V312 = request.form['V312']
-    V70 = request.form['V70']
-    V319 = request.form['V319']
-    V19 = request.form['V19']
-    V28 = request.form['V28']
-    V292 = request.form['V292']
-    V28_1 = request.form['V28_1']
-    V82 = request.form['V82']
-    V311 = request.form['V311']
-    V86 = request.form['V86']
-    V26 = request.form['V26']
-    V300 = request.form['V300']
+    TransactionDT = request.form['TransactionDT']
+    C1 = request.form['C1']
+    C3 = request.form['C3']
+    C5 = request.form['C5']
+    C6 = request.form['C6']
+    C7 = request.form['C7']
+    C8 = request.form['C8']
     C9 = request.form['C9']
-    V301 = request.form['V301']
-    V88 = request.form['V88']
-    V284 = request.form['V284']
-    V301_1 = request.form['V301_1']
-    V121 = request.form['V121']
-    V56 = request.form['V56']
-    D15 = request.form['D15']
-    V106 = request.form['V106']
-    V107 = request.form['V107']
-    addr2 = request.form['addr2']
+    C10 = request.form['C10']
+    C11 = request.form['C11']
+    C12 = request.form['C12']
+    C13 = request.form['C13']
+    C14 = request.form['C14']
+    card1 = request.form['card1']
+    card2 = request.form['card2']
+    card3 = request.form['card3']
     card5 = request.form['card5']
-    addr1 = request.form['addr1']
-    ProductCD_H = request.form['ProductCD_H']
-    ProductCD_R = request.form['ProductCD_R']
-    ProductCD_S = request.form['ProductCD_S']
-    ProductCD_W = request.form['ProductCD_W']
-    card4_discover = request.form['card4_discover']
-    card4_mastercard = request.form['card4_mastercard']
-    card4_visa = request.form['card4_visa']
-    card6_credit = request.form['card6_credit']
-    card6_debit = request.form['card6_debit']
-    card6_debit_or_credit = request.form['card6_debit_or_credit']
-    P_emaildomain_summary_anonymous_com = request.form['P_emaildomain_summary_anonymous_com']
+    D1 = request.form['D1']
+    D10 = request.form['D10']
     P_emaildomain_summary_aol_com = request.form['P_emaildomain_summary_aol_com']
-    P_emaildomain_summary_gmail_com = request.form['P_emaildomain_summary_gmail_com']
-    P_emaildomain_summary_hotmail_com = request.form['P_emaildomain_summary_hotmail_com']
-    P_emaildomain_summary_yahoo_com = request.form['P_emaildomain_summary_yahoo_com']
     Transaction_Amount_bucket_low = request.form['Transaction_Amount_bucket_low']
-    Transaction_Amount_bucket_medium = request.form['Transaction_Amount_bucket_medium']
+    V85 = request.form['V85']
+    V86 = request.form['V86']
+    V126 = request.form['V126']
+    V282 = request.form['V282']
+    V306 = request.form['V306']
+    V307 = request.form['V307']
+    V309 = request.form['V309']
+    V316 = request.form['V316']
+    addr1 = request.form['addr2']
 
-
-    print(f"V308 --> {V308}")
-    print(f"V57 --> {V57}")
-    print(f"C11 --> {C11}")
-    print(f"V114 --> {V114}")
-    print(f"V283 --> {V283}")
-    print(f"V312 --> {V312}")
-    print(f"V70 --> {V70}")
-    print(f"V319 --> {V319}")
-    print(f"V19 --> {V19}")
-    print(f"V28 --> {V28}")
-    print(f"V292 --> {V292}")
-    print(f"V28_1 --> {V28_1}")
-    print(f"V82 --> {V82}")
-    print(f"V311 --> {V311}")
     print(f"V86 --> {V86}")
-    print(f"V26 --> {V26}")
-    print(f"V300 --> {V300}")
-    print(f"C9 --> {C9}")
-    print(f"V301 --> {V301}")
-    print(f"V88 --> {V88}")
-    print(f"V284 --> {V284}")
-    print(f"V301_1 --> {V301_1}")
-    print(f"V121 --> {V121}")
-    print(f"V56 --> {V56}")
-    print(f"D15 --> {D15}")
-    print(f"V106 --> {V106}")
-    print(f"V107 --> {V107}")
-    print(f"addr2 --> {addr2}")
-    print(f"card5 --> {card5}")
-    print(f"addr1 --> {addr1}")
-    print(f"ProductCD_H --> {ProductCD_H}")
-    print(f"ProductCD_R --> {ProductCD_R}")
-    print(f"ProductCD_S --> {ProductCD_S}")
-    print(f"ProductCD_W --> {ProductCD_W}")
-    print(f"card4_discover --> {card4_discover}")
-    print(f"card4_mastercard --> {card4_mastercard}")
-    print(f"card4_visa --> {card4_visa}")
-    print(f"card6_credit --> {card6_credit}")
-    print(f"card6_debit --> {card6_debit}")
-    print(f"card6_debit_or_credit --> {card6_debit_or_credit}")
-    print(f"P_emaildomain_summary_anonymous_com --> {P_emaildomain_summary_anonymous_com}")
     print(f"P_emaildomain_summary_aol_com --> {P_emaildomain_summary_aol_com}")
-    print(f"P_emaildomain_summary_gmail_com --> {P_emaildomain_summary_gmail_com}")
-    print(f"P_emaildomain_summary_hotmail_com --> {P_emaildomain_summary_hotmail_com}")
-    print(f"P_emaildomain_summary_yahoo_com --> {P_emaildomain_summary_yahoo_com}")
-    print(f"Transaction_Amount_bucket_low --> {Transaction_Amount_bucket_low}")
-    print(f"Transaction_Amount_bucket_medium --> {Transaction_Amount_bucket_medium}")
 
 #    pred=model.predict(np.array([[V308,V57,C11,V114,V283,V312,V70,V319,V19,V28,V292,V28_1,V82,V311,V86,V26,V300,C9,V301,V88,V284,V301_1,V121,V56,D15,V106,V107,addr2,card5,addr1,ProductCD_H,ProductCD_R,ProductCD_S,ProductCD_W,card4_discover,card4_mastercard,card4_visa,card6_credit,card6_debit,card6_debit_or_credit,P_emaildomain_summary_anonymous_com,P_emaildomain_summary_aol_com,P_emaildomain_summary_gmail_com,P_emaildomain_summary_hotmail_com,P_emaildomain_summary_yahoo_com,Transaction_Amount_bucket_low,Transaction_Amount_bucket_medium]]))      
 #    pred = model.predict(np.array([[V308,V57,C11,V114,V283,V312,V70,V319,V19,V28,V292,V28_1,  V82,    V311,   V86,    V26,    V300,   C9, V301,   V88,    V284,   V301_1, V121,   V56,    D15,    V106,   V107,   addr2,  card5,  addr1,  ProductCD_H,    ProductCD_R,    ProductCD_S,    ProductCD_W,    card4_discover, card4_mastercard,   card4_visa, card6_credit,   card6_debit,    card6_debit_or_credit,  P_emaildomain_summary_anonymous_com,    P_emaildomain_summary_aol_com,  P_emaildomain_summary_gmail_com,    P_emaildomain_summary_hotmail_com,  P_emaildomain_summary_yahoo_com,    Transaction_Amount_bucket_low,  Transaction_Amount_bucket_medium]]))
 #    pred = model.predict(np.array([[1,0,0,0,0,1,0,0,2,0,2,0,2,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,1]]))
 
-    input_variables = pd.DataFrame([[V308,V57,C11,V114,V283,V312,V70,V319,V19,V28,V292,V28_1,V82,V311,V86,V26,V300,C9,V301,V88,V284,V301_1,V121,V56,D15,V106,V107,addr2,card5,addr1,ProductCD_H,ProductCD_R,ProductCD_S,ProductCD_W,card4_discover,card4_mastercard,card4_visa,card6_credit,card6_debit,card6_debit_or_credit,P_emaildomain_summary_anonymous_com,P_emaildomain_summary_aol_com,P_emaildomain_summary_gmail_com,P_emaildomain_summary_hotmail_com,P_emaildomain_summary_yahoo_com,Transaction_Amount_bucket_low,Transaction_Amount_bucket_medium]],
-                                   columns=['V308','V57','C11','V114','V283','V312','V70','V319','V19','V28','V292','V28_1','V82','V311','V86','V26','V300','C9','V301','V88','V284','V301_1','V121','V56','D15','V106','V107','addr2','card5','addr1','ProductCD_H','ProductCD_R','ProductCD_S','ProductCD_W','card4_discover','card4_mastercard','card4_visa','card6_credit','card6_debit','card6_debit_or_credit','P_emaildomain_summary_anonymous_com','P_emaildomain_summary_aol_com','P_emaildomain_summary_gmail_com','P_emaildomain_summary_hotmail_com','P_emaildomain_summary_yahoo_com','Transaction_Amount_bucket_low','Transaction_Amount_bucket_medium'],
+    input_variables = pd.DataFrame([[TransactionDT,card5,card1,addr2,C12,C13,C1,C10,C3,card3,D10,C7,V86,V85,C14,C9,C5,C11,V306,D1,C6,P_emaildomain_summary_aol_com,V309,V307,Transaction_Amount_bucket_low,V126,V282,V316,C8,card2]],
+                                   columns=['TransactionDT', 'card5', 'card1', 'addr2', 'C12', 'C13', 'C1', 'C10', 'C3', 'card3', 'D10', 'C7', 'V86', 'V85', 'C14', 'C9', 'C5', 'C11', 'V306', 'D1', 'C6', 'P_emaildomain_summary_aol_com', 'V309', 'V307','Transaction_Amount_bucket_low', 'V126', 'V282', 'V316', 'C8', 'card2'],
                                    dtype=float)
     pred = model.predict(input_variables)[0]
 
